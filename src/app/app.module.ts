ad828806e3bf04cf08acd09996fd34743d27b723
Malena de la Ossa <malenadelaossa@hotmail.com>
2020-07-23 21:12:19 -0600

    primercomponente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {holaMundo} from './primercomponente/primer.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    holaMundo
   ],
   imports: [
     BrowserModule,
